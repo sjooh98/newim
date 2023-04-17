442e09f8d63c525f0c0f41aae8ae9ec161f750cf
sjooh98 <charlo1209@gmail.com>
2023-04-17 14:41:00 +0900

    buildsuccess

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/snu/',
+  base: '/newim/',
   build: {
     // 프로젝트가 호스팅되는 GitHub Pages의 저장소 이름으로 경로를 설정합니다.
     // 예를 들어, 저장소 이름이 'my-project'라면 다음과 같이 설정합니다.
@@ -15,6 +15,6 @@ export default defineConfig({
         entryFileNames: `[name].[hash].js`,
       }
     },
-    publicPath: '/snu/'
+    publicPath: '/newim/'
   },
 });
